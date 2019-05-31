b86d6975c92a6160d404d09d9f54fee7c0fbf844
Felipe Andrés Maldonado Abello <felipe.maldonado19@outlook.com>
2019-05-31 15:28:18 -0400

    Recordatorio de $pokemon

--- cerdobot.js
+++ cerdobot.js
@@ -1,3 +1,4 @@
+
 const discord = require('discord.js');
 const client = new discord.Client();
 const botToken = "NTgzOTAwNDQyMTM2Njc0MzA2.XPDGKw.6x13PVWSAbkt_VoVQYQoQg71iYA";
@@ -9,6 +10,25 @@ const cer2 = {
       return client.channels.get("583470609388798089");
   }
 };
+
+function timeToMilliseconds(time){
+
+  var format = time.charAt(time.length - 1);
+  var time = parseInt(time.substring(0,time.length - 1),10);
+
+  time *= 1000;
+  if (format == 's') return time;
+  time *= 60;
+  if (format == 'm') return time;
+  time *= 60;
+  if (format == 'h') return time;
+  time *= 24;
+  if (format == 'd') return time;
+
+  console.log('Debes ingresar un formato para transformar a milisegundos');
+  return 1;
+
+}
 
 client.on('ready', () => {
   // List servers the bot is connected to
@@ -26,8 +46,19 @@ client.on('message', (message) => {
   if (message.author == client.user){ return;}
   
   if (message.channel.id == cer2.pruebaBots.id){
-    cer2.pruebaBots.send(message.author + " dijo " + message);
+    cer2.pruebaBots.send(message.author.toString() + " dijo " + message.content);
   }
+
+  if (message.content == '$pokemon'){
+
+    setTimeout(() => {
+
+      message.channel.send(message.author.toString() + " ya puedes usar $pokemon otra vez!");
+
+    }, timeToMilliseconds('2h'));
+
+  }
+
 });
   
 client.login(botToken);
