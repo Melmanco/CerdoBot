a5739553bfcba14b4bb902d2c242fbc922782a9d
Felipe Andrés Maldonado Abello <felipe.maldonado19@outlook.com>
2019-06-01 18:31:54 -0400

    Added an object that stores all users all users commands state to know if CerdoBot has to remind them to use it

--- cerdobot.js
+++ cerdobot.js
@@ -1,4 +1,3 @@
-
 const discord = require('discord.js');
 const client = new discord.Client();
 const botToken = "NTgzOTAwNDQyMTM2Njc0MzA2.XPDGKw.6x13PVWSAbkt_VoVQYQoQg71iYA";
@@ -10,6 +9,7 @@ const cer2 = {
       return client.channels.get("583470609388798089");
   }
 };
+var userList = {};
  
 // Transforms seconds, minutes, hours or days
 // with the form 'quantityFormat' to milliseconds 
@@ -40,6 +40,15 @@ client.on('ready', () => {
   })
   // Says hi to server
   //cer2.general.send("k onda k pex");
+
+  // Creates an object with all users $pokemon and pls work state
+  // to know if it has to remind them to use it
+  client.users.forEach((user) => {
+
+    userList[user] = {pokemon: false, work: false};
+
+  })
+
 })
 
 client.on('message', (message) => {
@@ -52,8 +61,11 @@ client.on('message', (message) => {
     cer2.pruebaBots.send(message.author.toString() + " dijo " + message.content);
   }*/
 
-  // Reminder for $pokemon
-  if (message.content == '$pokemon'){
+  // Reminder for $pokemon if haven't used $pokemon
+  if (message.content == '$pokemon' && !userList[message.author][pokemon]){
+
+    userList[message.author][pokemon] = true;
+    message.channel.send(message.author.toString() + ' te voy a recordar capturar un pokemon en dos horas');
 
     setTimeout(() => {
 
@@ -63,9 +75,11 @@ client.on('message', (message) => {
 
   }
 
-  // Reminder for pls work
-  if (message.content == 'pls work'){
+  // Reminder for pls work if haven't used pls work
+  if (message.content == 'pls work' && !userList[message.author][work]){
 
+    userList[message.author][pokemon] = true;
+    message.channel.send(message.author.toString() + ' te voy a recordar trabajar en una hora');
     setTimeout(() => {
 
       message.channel.send(message.author.toString() + ' ya puedes trabajar de nuevo!');
